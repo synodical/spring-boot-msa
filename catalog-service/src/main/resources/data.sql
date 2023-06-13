7c596b63115d7143229eff118ed6f3b9ef06e4d3
synodical <lena0323park@naver.com>
2023-06-13 14:02:46 +0900

    catalog-service 완수

---
+++ catalog-service/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+-- CREATE TABLE catalog (
+--                          product_id VARCHAR(50) PRIMARY KEY,
+--                          product_name VARCHAR(100),
+--                          stock INT,
+--                          unit_price DECIMAL(10,2)
+-- );
+
+
+
+insert into catalog(product_id, product_name, stock, unit_price)
+values ('CATALOG_001', 'Berlin', 100, 1500);
+
+insert into catalog(product_id, product_name, stock, unit_price)
+values ('CATALOG_002', 'Berlin', 100, 1500);
+
+insert into catalog(product_id, product_name, stock, unit_price)
+values ('CATALOG_003', 'Berlin', 100, 1500);
